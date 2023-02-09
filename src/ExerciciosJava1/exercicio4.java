16cae6604197e5fce58152d1f46099f5d1edbf8d
GabiSilvaBelo <gabybelosp@gmail.com>
2023-02-09 10:28:50 -0300

    Exercicios Java 1 ao 4

---
+++ src/ExerciciosJava1/exercicio4.java
@@ -0,0 +1,25 @@
+package ExerciciosJava1;
+
+import java.util.Scanner;
+
+public class exercicio4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n1=5;
+		int n2=6;	
+		int n3=-7;
+		int n4=8;
+		
+
+		Scanner leia = new Scanner(System.in);
+		
+		int subtracao=(n1*n2)-(n3*n4);
+			
+		System.out.println("\nA diferença é de: "+subtracao);
+		
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author vinil
+ *
+ */
+module ProjetoGen {
+}
