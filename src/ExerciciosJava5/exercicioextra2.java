648ee563a1f7b96ae79c8b92a522ee082358e3e7
GabiSilvaBelo <gabybelosp@gmail.com>
2023-02-13 16:39:21 -0300

    Exercicios extras 1, 2 e 3

---
+++ src/ExerciciosJava5/exercicioextra2.java
@@ -0,0 +1,28 @@
+package ExerciciosJava5;
+
+import java.util.Scanner;
+
+public class exercicioextra2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner leia = new Scanner(System.in);
+
+		int i, contPar = 0, contImpar = 0, num;
+
+		for (i = 1; i <= 10; i++) {
+			System.out.println("Digite um número inteiro: ");
+			num = leia.nextInt();
+			if (num%2==0) {
+				contPar++;
+			}
+			else if (num%2!=0) {
+				contImpar++;
+			        
+		}
+	
+		}
+		System.out.println("Foram digitados " + contPar + " número par(es) e " + contImpar + " ímpar(es)");
+	}
+}
